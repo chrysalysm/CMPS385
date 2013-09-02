1150cd6b170c20961c11498aadb522c1e7457f09
Benjamin Chu <allthelosersare@gmail.com>
2013-09-01 20:06:41 -0700

    Removing gitignore from Project1c

--- Project1c/src/Project1c.cpp
+++ Project1c/src/Project1c.cpp
@@ -7,6 +7,7 @@
 //============================================================================
 
 #include <iostream>
+#include <cctype>
 using namespace std;
 
 int main() {
@@ -19,5 +20,22 @@ int main() {
 		cout << " ";
 	}
 	cout << endl;
+
+
+	Days[5][3] = 'u';
+	Days[5][4] = 'r';
+	Days[5][5] = 'd';
+	Days[5][6] = 'a';
+	Days[5][7] = 'y';
+
+
+	for (int a = 0; a < 7; a++) {
+			for (int b = 0; b < 15; b++) {
+				cout << Days[a][b];
+			}
+			cout << " ";
+		}
+	cout << endl;
+
 	return 0;
 }
