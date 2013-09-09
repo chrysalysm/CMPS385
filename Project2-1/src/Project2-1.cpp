c7b1246e3abebf0eca6c07a09c783a7c6c2252b2
Benjamin Chu <allthelosersare@gmail.com>
2013-09-09 02:55:34 -0700

    Added Project 2-2.  Completed Project 2-1 and 2-2.

--- Project2-1/src/Project2-1.cpp
+++ Project2-1/src/Project2-1.cpp
@@ -3,13 +3,52 @@
 // Author      : Benjamin Chu
 // Version     :
 // Copyright   : Creative Commons - Attribution 3.0
-// Description : Hello World in C++, Ansi-style
+// Description : Project2-1 - <algorithm> sort()
 //============================================================================
 
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 
+// Template class to handle Display function without overloading
+template <class T>
+
+void Display(T x[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << x[i] << " ";
+	}
+	cout << endl;
+}
+
 int main() {
-	cout << "Hello World!" << endl; // prints Hello World!
+
+	// Declaring variables
+	int a[5] = {3, 8, 1, 9, 2};
+	string day[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+	char c[9] = {'s', 'e', 'p', 't', 'e', 'm', 'b', 'e', 'r'};
+
+	cout << endl << "Array a[] before being sorted: ";
+	Display(a, 5);  // sizeof(a)/sizeof(a[0])
+	// Sorting Array a[]
+	sort(a, a + 5);
+	cout << "Array a[] after being sorted: ";
+	Display(a, 5);
+
+	cout << "Array day[] before being sorted: ";
+	Display(day, 7);  // sizeof(day)/sizeof(day[0])
+	// Sorting Array day[]
+	sort(day, day + 7);
+	cout << "Array day[] after being sorted: ";
+	Display(day, 7);
+
+	cout << "Array c[] before being sorted: ";
+	Display(c, 9);  // sizeof(c)/sizeof(c[0])
+	// Sorting Array c[]
+	sort(c, c + 9);
+	cout << "Array c[] after being sorted: ";
+	Display(c, 9);
+
+	cout << endl;
 	return 0;
 }
---
+++ Project2-2/src/Project2-2.cpp
@@ -0,0 +1,39 @@
+//============================================================================
+// Name        : Project2-2.cpp
+// Author      : Benjamin Chu
+// Version     :
+// Copyright   : Creative Commons - Attribution 3.0
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+// Declaring user-defined functions
+int sum(int, int);
+int g(int);
+
+int main() {
+	cout << endl;
+	int start, finish;
+	cout << sum(3, 8) << endl;
+	start = clock();
+	cout << g(41) << endl;
+	finish = clock();
+	cout << "Runtime for g(n) is: " << (finish - start) / CLK_TCK << endl;
+	cout << endl;
+	return 0;
+}
+
+// Defining sum()
+int sum(int n, int m) {
+	if (n == m) return m;
+	else return n + sum(n + 1, m);
+}
+
+// Defining g()
+int g(int n) {
+	if (n <= 3) return 2;
+	else return n + g(n - 1) + g(n - 2);
+}
